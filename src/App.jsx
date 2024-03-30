c51cbad298e06c4e9e8f4a9dd6712388cbe8e09e
ShahinFallah <shahinfallah2006@gmail.com>
2024-03-31 00:47:01 +0330

    add TimeAndLocation Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons'
 import Inputs from './components/Inputs'
+import TimeAndLocation from './components/TimeAndLocation'
 
 export default function App() {
   return (
@@ -8,6 +9,8 @@ export default function App() {
       <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
         <TopButtons />
         <Inputs />
+
+        <TimeAndLocation />
       </div>
     </>
   )
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+
+export default function TimeAndLocation() {
+  return (
+    <>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-slate-300 text-xl font-extralight">
+          Tuesday, 31 May 2022 | Local time: 12.46 PM
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">
+          Berlin, DE
+        </p>
+      </div>
+    </>
+  )
+}
